7c9f3c3c546d326b75727e860cfc05f876998552
宋 锦辉 <sjh@wifisec.com.cn>
2021-09-23 21:25:10 +0800

    新增设置cookie窗口

--- SecKill/MainWindow.xaml.cs
+++ SecKill/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using SecKill.Windows;
+using config = SecKill.Config.Config;
 
 namespace SecKill
 {
@@ -21,12 +23,47 @@ namespace SecKill
 
         private void City_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            ItemCollection collection = City.Items;
-            collection.Clear();
-
             string provinceName = Province.SelectedValue.ToString();
             List<Area> cities = areas.First(p => p.Value == provinceName)?.Children;
             City.ItemsSource = cities;
+        }
+
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            string provinceName = Province.SelectedValue.ToString();
+            string cityName = City.SelectedValue.ToString();
+            if (provinceName.Length == 0 || cityName.Length == 0)
+            {
+                MessageBox.Show("省份或城市不能为空");
+            }
+            else
+            {
+                config.RegionCode = cityName;
+                Area city = (Area)City.SelectedItem;
+                MessageBox.Show($"已选择区域:{city.Name}");
+            }
+        }
+
+        private void SettingCookie_Click(object sender, RoutedEventArgs e)
+        {
+            SettingCookieWindow cookiePage = new SettingCookieWindow();
+            cookiePage.Title = "设置抢购参数";
+            cookiePage.Show();
+        }
+
+        private void SwitchMember_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RefreshVaccineList_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StartKill_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ SecKill/Windows/SettingCookieWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using config = SecKill.Config.Config;
+
+namespace SecKill.Windows
+{
+    /// <summary>
+    /// SettingCookieWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class SettingCookieWindow : Window
+    {
+        public SettingCookieWindow()
+        {
+            InitializeComponent();
+            reqHeader.Text = config.RequestHeader;
+        }
+
+        private void ParseButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
