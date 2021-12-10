4c19ebca7dad567a153c62edeb5b18e8ead51be1
SinnoSong <song.song@outlook.com>
2021-12-10 16:58:55 +0800

    记录问题

--- SecKill/Service/HttpService.cs
+++ SecKill/Service/HttpService.cs
@@ -69,6 +69,7 @@ namespace SecKill.Service
         /// <returns></returns>
         public string GetSt(string vaccineId)
         {
+            // todo json转换出现异常
             string path = baseUrl + "/seckill/seckill/checkstock2.do";
             Dictionary<string, string> urlParams = new Dictionary<string, string>();
             urlParams.Add("id", vaccineId);
